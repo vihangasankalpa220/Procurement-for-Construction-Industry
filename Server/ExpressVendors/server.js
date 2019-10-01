e8d8cfaec2545c66b699e880fdbfc896a855efd9
Hewage, Asiri <asiri.hewage@pearson.com>
2019-10-01 23:20:13 +0530

    bug fixes

--- Server/ExpressVendors/server.js
+++ Server/ExpressVendors/server.js
@@ -1,19 +1,25 @@
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
-const config = require('./DB.js');
 const businessRoute = require('./routes/business.route');
 const stockssRoute = require('./routes/stock.route');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://asiri:asiri123@cluster0-lok9v.mongodb.net/procurementDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const itemsCollectio = client.db("procurementDB").collection("items");
+  const vendorsCollectio = client.db("procurementDB").collection("vendors");
+  const usersCollectio = client.db("procurementDB").collection("users");
+  const purchaseCollectio = client.db("procurementDB").collection("purchase");
+  const vendorItemsCollectio = client.db("procurementDB").collection("vendorItems");
+  console.log('MongoDB Connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,7 +29,7 @@ app.use('/business', businessRoute);
 
 app.use('/stock', stockssRoute);
 
-
+var PORT = process.env.PORT || 5000
 
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
